904628046bdd95a01456ce4ddb253d54ddcc6cb5
Ilyar Hamid <ilyarhamid@Ilyars-MacBook-Pro.local>
2020-06-28 23:23:07 +0200

    final code 1.0 version

--- table/game.py
+++ table/game.py
@@ -21,20 +21,18 @@ value_dict = {
 
 class Game(object):
     def __init__(self,
-                 dealer: Player,
-                 players: List[Player],
-                 key_player: Player,
-                 initial_deck: Deck,
-                 rule: str,
-                 betting: str,
-                 black_jack_pay: float,
-                 shoe_pct: float,
-                 log: bool,
-                 ) -> None:
-        self.dealer = dealer
-        self.players = players
-        self.key_player = key_player
-        self.deck = initial_deck
+                 n_pair: int = 8,  # How many pairs of cards in the deck
+                 n_player: int = 3,  # Number of mock players
+                 rule: str = "H17",  # H17: Dealer must hit on soft 17; S17: Dealer stands on soft 17.
+                 betting: str = "flat",  # flat: flat betting; MIT: MIT BlackJack team betting strategy
+                 black_jack_pay: float = 3 / 2,  # Usually 3/2 or 6/5
+                 shoe_pct: float = 0.3,
+                 log: bool = False):  # When to shuffle the shoe
+        self.dealer = Player()
+        self.players = [Player() for i in range(n_player)]
+        self.key_player = Player()
+        self.deck = Deck(n_pair)
+        self.deck.shuffle()
         self.rule = rule
         self.betting = betting
         self.black_jack_pay = black_jack_pay
@@ -67,12 +65,11 @@ class Game(object):
 
     def single_player(self, p: Player, flag=False) -> None:
         self.player_split(p)
-        if self.log:
-            print("Player Hands Split!")
         h_count = 1
         for h in p.hands:
-            if self.log:
+            if self.log and flag:
                 print(f"Hand {h_count}")
+                h_count += 1
             if flag:
                 if self.betting == "MIT":
                     true_count = self.deck.count / round(len(self.deck.deck) / 52.0)
@@ -88,27 +85,27 @@ class Game(object):
                 act = h.get_action(value_dict[self.dealer.hands[0].cards[0].value])
                 if act == "H":
                     h.cards.append(self.deck.deal())
-                    if self.log:
+                    if self.log and flag:
                         print(f"Hit: {[c.value for c in h.cards]}")
                 if act == "S":
-                    if self.log:
+                    if self.log and flag:
                         print(f"Stand: {[c.value for c in h.cards]}")
                     break
                 if act == "D":
                     h.cards.append(self.deck.deal())
                     p.money -= h.bet
                     h.bet = h.bet * 2.0
-                    if self.log:
+                    if self.log and flag:
                         print(f"Double: {[c.value for c in h.cards]}")
                     break
                 if act == "R":  # In case of surrender,
                     p.money += 0.5 * h.bet  # return half of the betting money to the player
                     h.bet = -0.5 * h.bet  # negative value is for marking purpose. This will be turned to positive value
-                    if self.log:
+                    if self.log and flag:
                         print(f"Surrender: {[c.value for c in h.cards]}")
                     break
                 if act == "Bust":
-                    if self.log:
+                    if self.log and flag:
                         print(f"Bust: {[c.value for c in h.cards]}")
                     break
 
@@ -118,8 +115,8 @@ class Game(object):
         self.single_player(self.key_player, flag=True)
         icount = 1
         for ply in self.players:
-            if self.log:
-                print(f"----------------- Player {icount} Turn ---------------")
+            # if self.log:
+            #     print(f"----------------- Player {icount} Turn ---------------")
             self.single_player(ply)
             icount += 1
 
@@ -161,16 +158,23 @@ class Game(object):
             if hand_value == 21 and len(h.cards) == 2:
                 if not dealer_black_jack:
                     h.bet = h.bet * (self.black_jack_pay + 1.0)
+                    # print("------------Player Win (Black Jack)------------")
             elif hand_value > 21:
+                # print("------------Dealer Win------------")
                 h.bet = 0
             else:
                 if dealer_value > 21:
                     h.bet = h.bet * 2.0
+                    # print("------------Player Win------------")
                 else:
                     if dealer_value > hand_value:
                         h.bet = 0.0
+                        # print("------------Dealer Win------------")
                     elif dealer_value < hand_value:
                         h.bet = h.bet * 2.0
+                        # print("------------Player Win------------")
+                    # else:
+                        # print("------------Push------------")
             self.key_player.money += h.bet
         #       Clear Hands
         self.dealer.hands = [Hand([])]
